ffa9242e2d00b5e710d1691ba2634cc806948d85
zeusul <86463455+zeusul@users.noreply.github.com>
2024-07-26 16:07:04 +1000

    connect to database

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import 'dotenv/config.js';
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}))
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Sever running on http://localhost:8080/');
+})
+
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB); // need your own Mongoose cluster url
+mongoose.connection.on('error', (error) => console.log(error));
