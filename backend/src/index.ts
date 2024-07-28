05fc0ee52861fe8b5cd9e40d2b06f6614aa82f07
jiale-wang-i <haidaowangpeanut@gmail.com>
2024-07-28 21:59:52 +0800

    auth

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,15 @@ dotenv.config()
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import authRoutes from "./routes/auth";
+import auth from "./middleware/auth";
 
 const app = express();
 
 app.use(cors({ credentials: true, }))
 app.use(express.json());
+
+app.use('/auth', authRoutes);
 
 app.listen(process.env.PORT || 8080, () => {
     console.log('app listening on http://localhost:8080/');
@@ -17,3 +21,8 @@ app.listen(process.env.PORT || 8080, () => {
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB);
 mongoose.connection.on('error', (error: Error) => console.log(error));
+
+// Example of a protected route
+app.get('/protected', auth, (req, res) => {
+    res.status(200).json({ message: "This is a protected route" });
+});
